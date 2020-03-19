ece2621bd414d546a414431c8af39582480a323e
rodrigoimparato <rodrigoimparato@gmail.com>
2020-03-19 20:05:43 -0300

    Subindo o fonte no GitHub

---
+++ src/Empresa.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Empresa.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        //Com parametro
+        public string EmailDomain { get; set; } = "dominio";
+
+        //Sem parametro
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + EmailDomain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
